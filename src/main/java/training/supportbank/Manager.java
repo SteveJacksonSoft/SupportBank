4d1156fadf2e4e4a243a5783c67a7054d562bf6f
smudge37 <stephen.jackson@softwire.com>
2019-02-28 13:57:10 +0000

    Random commit

--- src/main/java/training/supportbank/Manager.java
+++ src/main/java/training/supportbank/Manager.java
@@ -2,6 +2,8 @@ package training.supportbank;
 
 import java.io.IOException;
 import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Manager {
     private final UserInterface ui = new UserInterface();
@@ -16,26 +18,35 @@ public class Manager {
     }
 
     public void run() {
+        List<Integer> list = new ArrayList<>();
         this.updateBank();
+        ui.welcome();
         Command command = ui.getCommand();
         if (command instanceof QuitCommand) {
             System.out.println("Programme exiting.");
             System.exit(-1);
         } else if (command instanceof ListSpecificAccountCommand) {
-            try {
-                bank.list(((ListSpecificAccountCommand) command).getAccountName());
-            } catch (InvalidParameterException e) {
-                System.out.println(e.getMessage());
-                run();
-            }
+            this.executeListCommand(((ListSpecificAccountCommand) command).getAccountName());
         } else if (command instanceof ListAllCommand){
-            bank.listAll();
+            this.executeListAllCommand();
         } else {
             System.out.println("You have not entered a valid command. Please try again.");
             run();
         }
     }
 
+    private void executeListAllCommand() {
+        bank.listAll();
+    }
+
+    private void executeListCommand(String accountName) {
+        try {
+            bank.list(accountName);
+        } catch (InvalidParameterException e) {
+            System.out.println(e.getMessage());
+            run();
+        }
+    }
 
     private void updateBank() {
         for (String filePath: recordFiles) {
